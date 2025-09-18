516a9af291ac30ad71eef02b9160eb07f96f0f39
Vlad Komarenets <komarenetsv@gmail.com>
2025-09-18 22:43:51 +0300

    Initial homework setup with CMake and Conan

---
+++ homework08/examples/basic_usage.cpp
@@ -0,0 +1,7 @@
+#include "core/project.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Example usage of ProjectManager core library\n";
+    return 0;
+}
---
+++ homework08/tests/test_core.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Smoke, TrueIsTrue) {
+    EXPECT_TRUE(true);
+}
